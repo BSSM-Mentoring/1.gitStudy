7f13ed8cf36a0080c5f8d966f0cc138ad18ab85b
Taehoo <taehug662@gmail.com>
2025-07-25 23:43:32 +0900

    feat/#4 : 김태후 추가

--- start.c
+++ start.c
@@ -3,8 +3,8 @@ int main(int argc, char* argv[]) {
   printf("hello");
   printf("김민재");
   printf("김태후");
-  printf("입니다.");
-  printf("아ㅏ아");
+  printf("김태후");
+  printf("rlaxogn");
   //fdsaffds
   return 0;
 }
